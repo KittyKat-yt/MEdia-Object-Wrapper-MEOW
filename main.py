d596291369e15921bf84e9e34e4e38381c9cd512
Kitty Kat :3 <159808900+KittyKat-yt@users.noreply.github.com>
2026-03-23 11:34:13 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from meow import MEOW
+
+# MEdia Object Wrapper
+# .meow
+
+meow = MEOW()
+
+meow.addFile("/file01")
+meow.addFile("/file02")
+meow.addFile("/file03")
+meow.combineFiles("stuff")
+
+#meow.separateFiles("stuff.meow")
